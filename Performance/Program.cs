60d543713a9fa73b96679b045413bcecc3789ef4
Thomas <devattom@gmail.com>
2026-04-20 13:21:34 +0200

    init projet optimiser

--- Performance/Program.cs
+++ Performance/Program.cs
@@ -8,25 +8,25 @@ var sw2 = Stopwatch.StartNew();
 
 double sum = 1;
 
-for (int j = 0; j < 10; j++)
+Parallel.For(1, 10, (num, state) =>
 {
+    Console.WriteLine($"Début du thread {num}");
     for (int i = 0; i < 50_000_000; i++)
     {
         sum += Math.Sin(i) + Math.Cos(i);
 
         sum += Math.Sqrt(i);
-    
+
         sum += Math.Exp(i % 10) + Math.Log(i + 1);
-    
+
         sum += Math.Pow(i % 100, 3);
 
         sum *= 1.00000001;
     }
     sw2.Stop();
-    Console.WriteLine($"Temps de calcul intermédiaire : {sw2.ElapsedMilliseconds} ms");
+    Console.WriteLine($"Temps de calcul thread : {num} : {sw2.ElapsedMilliseconds} ms");
     sw2.Restart();
-}
-
+});
 
 sw1.Stop();
 
